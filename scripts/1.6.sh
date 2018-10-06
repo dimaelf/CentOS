ccbeaf9f18c398403dfa39994a6a0c0a57e13084
dimaelf <dimakndratiev@mail.ru>
2018-10-07 02:49:59 +0300

    1.6 - count files and show well

--- scripts/1.6.sh
+++ scripts/1.6.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo 'Домашний каталог пользователя'
+whoami
+echo 'содержит обычных файлов:'
 ls ~/ -p | grep -v / | wc -l
+echo 'скрытых файлов:'
 ls ~/ -p -a | grep -v / | grep ^'\.' | wc -l
 
