7ba40b8a6e3b400740df72a7ab1e54fa40a94a70
dimaelf <dimakndratiev@mail.ru>
2018-10-10 22:01:05 +0300

    2.3 - zeros/nozeros

--- scripts/2.3.sh
+++ scripts/2.3.sh
@@ -2,14 +2,14 @@
 
 Z=/tmp/zeros
 NOZ=/tmp/nozeros
-cat ~/bash.txt | grep 000000 > $Z
-cat ~/bash.txt | grep -v 000000 | grep -v '\*' > $NOZ
+<  ~/bash.txt grep 000000 > $Z
+<  ~/bash.txt grep -v 000000 | grep -v '\*' > $NOZ
 echo $Z
-cat $Z | head -n 10
+< $Z head -n 10
 echo '........'
-cat $Z | sort -r | head -n 10 | sort
+< $Z sort -r | head -n 10 | sort
 echo ''
 echo $NOZ
-cat $NOZ | head -n 10
+< $NOZ head -n 10
 echo '........'
-cat $NOZ | sort -r | head -n 10 | sort
+< $NOZ sort -r | head -n 10 | sort
